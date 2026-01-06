646315db3ae2b1085a2e367321e453a3dcc1e56e
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-06 17:17:46 +0100

    nestjs

---
+++ firstnest/src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    getUser(): string {
+        return this.userService.getUser();
+    }
+}
---
+++ firstnest/src/user/user.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({})
+export class UserModule {}
