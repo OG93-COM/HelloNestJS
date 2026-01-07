62713c3f53a9520f5dd96360ef8a7aec67990345
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-07 16:54:19 +0100

    nestjs

--- firstnest/src/app.module.ts
+++ firstnest/src/app.module.ts
@@ -7,7 +7,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- firstnest/src/user/user.service.ts
+++ firstnest/src/user/user.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
     getUser(): string {
-        return "User Controller is working";
+        return "User Controller is working now";
     }
 }
